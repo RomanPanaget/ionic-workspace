8c8c511db773d753e1d5fda0f39070420b9d110a
RomainYoobic <rpanaget@yoobic.com>
2020-01-23 16:42:39 +0000

    feat(app): added lib1 to app1

--- projects/app1/src/app/tab1/tab1.module.ts
+++ projects/app1/src/app/tab1/tab1.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
+import { Lib1Module } from '@lib1';
 
 @NgModule({
   imports: [
     IonicModule,
+    
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab1Page }])
+    RouterModule.forChild([{ path: '', component: Tab1Page }]),
+    Lib1Module
   ],
   declarations: [Tab1Page]
 })
